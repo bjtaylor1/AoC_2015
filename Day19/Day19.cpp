27904f9c0ea7d401c8bb210e16a0fe530e62c368
Ben Taylor <ben.taylor@ssp-uk.com>
2016-12-17 20:37:53 +0000

    day 19 cpp sorts with int and string

--- Day19/Day19.cpp
+++ Day19/Day19.cpp
@@ -6,39 +6,37 @@
 class c
 {
 public:
-	//std::string s;
+	std::string s;
 	int i;
-	c(const int _i /*, const std::string _s*/) : i(_i) /*, s(_s)*/ {}
+	c(const int _i , const std::string _s) : i(_i) , s(_s) {}
 	bool operator<(const  c& other) //(1)
 	{
-		return i < other.i /*|| s < other.s*/;
+		if (i != other.i) return i < other.i;
+		return s < other.s;
 	}
 };
 
-/*
+
 bool iThenS(const c& c1, const c& c2)
 {
-	if (c1.i < c2.i) return true;
-	bool stringComp = c1.s < c2.s;
-	return stringComp;
+	if (c1.i != c2.i) return c1.i < c2.i;
+	return c1.s < c2.s;
 }
-*/
+
 
 std::ostream& operator<<(std::ostream& os, const c& c)
 {
-	os << c.i /*<< "; " << c.s*/;
+	os << c.i << "; " << c.s;
 	return os;
 }
 
 int main()
 {
-	std::cout << "hello3" << std::endl;
-
 	std::vector<c> vec;
-	vec.push_back(c(42 /*, "def"*/));
-	vec.push_back(c(42 /*, "abc"*/));
-	vec.push_back(c(715 /*, "abc"*/));
-	//std::sort(vec.begin(), vec.end());
+	vec.push_back(c(42 , "def"));
+	vec.push_back(c(42 , "abc"));
+	vec.push_back(c(715 , "abc"));
+	std::sort(vec.begin(), vec.end(), iThenS);
 	for (std::vector<c>::iterator it = vec.begin(); it != vec.end(); it++)
 	{
 		std::cout << (*it) << std::endl;
